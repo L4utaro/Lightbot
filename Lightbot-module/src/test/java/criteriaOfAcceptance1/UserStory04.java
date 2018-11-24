688d7d3a02e324fecff9ea05bb582cee6c239c26
Lautaro <lautarodebenedetti@gmail.com>
2018-11-24 17:23:16 -0300

    user

--- Lightbot-module/src/test/java/criteriaOfAcceptance1/UserStory04.java
+++ Lightbot-module/src/test/java/criteriaOfAcceptance1/UserStory04.java
@@ -35,12 +35,16 @@ public class UserStory04 {
 	 * commandMove, commandMove], como todas las instrucciones son válidas, se
 	 * deberán ejecutar las instrucciones. Pero como el avatar termina saliendo del
 	 * camino posible se deberá lanzar un IllegalArgumentException, indicando que el
-	 * avatar salió del camino posible.
+	 * avatar salió del camino posible. En la 5 instrucción, el avatar sale del juego, 
+	 * por lo tanto tardaría menos de 6 segundos la ejecución.
+
 	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void checkActionsTest02() {
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 		this.game = new Game(ConstantsTest.ROUTE_MAP_PROPERTIES, ConstantsTest.ROUTE_JSON_ACTIONS_INVALID_2);
 		this.game.run();
+		assertTrue((new Timestamp(System.currentTimeMillis())).getTime() - (timestamp.getTime()) < 6000);
 	}
 	/*
 	 * Forma del mapa: 
@@ -51,30 +55,5 @@ public class UserStory04 {
 	 * [ WALK    ][ NO_WALK ][ NO_WALK   ] 
 	 * [ WALK    ][ WALK    ][ AVATAR    ] 
 	 * [ NO_WALK ][ WALK    ][LIGHT_OFF  ]
-	 */
-
-	/**
-	 * Leer la colección de acciones [commandRight, commandMove, commandLeft,
-	 * commandMove, commandRight, commandMove, commandLeft, commandMove], como todas
-	 * las instrucciones son válidas, se deberá corroborar que el avatar no salga
-	 * del camino posible, como no sale del camino, se verificará que todas las
-	 * luces están encendidas, pero como quedo una luz sin encender, entonces se
-	 * deberá lanzar un IllegalArgumentException, indicando que alguna luz no se
-	 * encuentran encendida.
-	 */
-	@Test(expected = IllegalArgumentException.class)
-	public void checkActionsTest03() {
-		this.game = new Game(ConstantsTest.ROUTE_MAP_PROPERTIES, ConstantsTest.ROUTE_JSON_ACTIONS_2);
-		this.game.run();
-	}
-	/*
-	 * Forma del mapa: 
-	 * [ AVATAR  ][ NO_WALK ][ NO_WALK   ] 
-	 * [ WALK    ][ WALK    ][ NO_WALK   ]
-	 * [ NO_WALK ][ WALK    ][ LIGHT_OFF ] 
-	 * Despues de ejecutar las acciones: 
-	 * [ WALK    ][ NO_WALK ][ NO_WALK   ] 
-	 * [ WALK    ][ WALK    ][ AVATAR    ] 
-	 * [ NO_WALK ][ WALK    ][ AVATAR    ]
 	 */
 }
