2d1b3e8be9cb1c46dff35d3de60131c1a41323d9
Lautaro <lautarodebenedetti@gmail.com>
2018-11-24 21:07:45 -0300

    refactor

--- Lightbot-view/src/main/java/views/PlayersView.java
+++ Lightbot-view/src/main/java/views/PlayersView.java
@@ -22,11 +22,11 @@ public class PlayersView implements Observer {
 	public PlayersView() {
 		this.frame = new JFrame("Lightbot");
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.frame.setLocationRelativeTo(null);
 		this.size = new Size(1200, 900);
 		this.frame.setSize(this.size.getHigh(), this.size.getWidht());
 		this.panel = new JPanel();
 		this.panel.setLayout(null);
+		this.frame.setLocationRelativeTo(null);
 	}
 
 	public void addPanelsPlayers(List<Player> players) {
--- Lightbot-view/src/test/java/criteriaOfAcceptance2/UserStory04.java
+++ Lightbot-view/src/test/java/criteriaOfAcceptance2/UserStory04.java
@@ -37,4 +37,21 @@ public class UserStory04 {
 		this.controller.runGames();
 		Thread.sleep(10000);
 	}
+	
+	/*
+	 * Cargar las acciones de la carpeta ActionsTest en el directorio
+	 * "src/main/resources/user04test03". Debera de cargar y mostrar todas las
+	 * partidas de los players y mostrar el resultado final de cada partida. En el
+	 * primer juego debera de mostrar el mensaje (You Win). Y el segundo juego (You win).
+	 * Pero como el segundo jugador utilizo menos instrucciones, debera mostrar por 
+	 * pantalla "Thw winner is player 2".
+	 */
+
+	@Test
+	public void test03() throws InterruptedException {
+		this.controller = new ControllerGames("src/main/resources/user04test03");
+		this.controller.initGames();
+		this.controller.runGames();
+		Thread.sleep(10000);
+	}
 }
